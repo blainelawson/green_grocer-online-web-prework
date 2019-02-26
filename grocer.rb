730b22183a3eb631cbc2ebd6c032f30f4a9eb3c9
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-26 17:47:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,66 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  items_array = [] 
+  
+  items_array = cart.map {|items_hash| items_hash.keys}.flatten
+  
+  items_array.each do |item|
+    item_count = items_array.count(item)
+    
+    cart.each do |items_hash|
+      items_hash.each do |key, value|
+        
+        item_values = {}
+          if item == key
+            value[:count] = item_count
+            item_values = value
+            
+            consolidated_cart[item] = item_values
+            break       #breaks after first instance of item because all values for that item remain the same
+          end
+        end
+    end
+  end
+    
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+  couponed_item_count = 0
+  couponed_item_array = []
+
+  coupons.each do |coupon_hash|
+    couponed_item = {}
+
+      # binding.pry      
+      item_name = coupon_hash[:item]
+   
+      if cart[item_name]  && cart[item_name][:count] >= coupon_hash[:num]
+binding.pry
+        if cart["#{item_name} W/COUPON"]
+          binding.pry
+          couponed_item["#{item_name} W/COUPON"][:count] += 1
+          # binding.pry
+        else
+          couponed_item["#{item_name} W/COUPON"] = {
+                                          price: coupon_hash[:cost],
+                                          clearance: cart[item_name][:clearance],
+                                          count: 1
+                                        }
+                                        # binding.pry
+        end
+
+      cart[item_name][:count] -= coupon_hash[:num]
+      cart["#{item_name} W/COUPON"] = couponed_item["#{item_name} W/COUPON"]
+      # binding.pry
+      end
+    end
+    # binding.pry
+    cart
+
 end
 
 def apply_clearance(cart)
