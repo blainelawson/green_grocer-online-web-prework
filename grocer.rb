c04b67e518de3c4b787fe46c07923e38624558b8
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-26 20:27:38 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,95 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  items_array = [] 
+  
+  items_array = cart.map {|items_hash| items_hash.keys}.flatten
+  
+  items_array.each do |item|
+    item_count = items_array.count(item)
+    
+    cart.each do |items_hash|
+      items_hash.each do |key, value|
+        
+        item_values = {}
+          if item == key
+            value[:count] = item_count
+            item_values = value
+            
+            consolidated_cart[item] = item_values
+            break       #breaks after first instance of item because all values for that item remain the same
+          end
+        end
+    end
+  end
+    
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+  couponed_item_count = 0
+  
+  coupons.each do |coupon_hash|
+    couponed_item = {}
+
+    item_name = coupon_hash[:item]
+   
+    if cart[item_name]  && cart[item_name][:count] >= coupon_hash[:num]
+
+      if cart["#{item_name} W/COUPON"]
+          
+        cart["#{item_name} W/COUPON"][:count] += 1
+      else
+        couponed_item["#{item_name} W/COUPON"] = {
+                                        price: coupon_hash[:cost],
+                                        clearance: cart[item_name][:clearance],
+                                        count: 1
+                                        }
+        cart["#{item_name} W/COUPON"] = couponed_item["#{item_name} W/COUPON"]                                        
+      end
+
+      cart[item_name][:count] -= coupon_hash[:num]
+    end
+  end
+  
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  # binding.pry
+ 
+  cart.each do |item, values|
+    # binding.pry
+    if values[:clearance] == true
+      price = (values[:price] * 0.80).round(2)
+      cart[item][:price] = price
+    end
+  end
+  
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  # binding.pry
+  total = 0
+  consolidated_cart = consolidate_cart(cart)
+  
+  cart_with_coupons = apply_coupons(consolidated_cart, coupons)
+  
+  cart_result = apply_clearance(cart_with_coupons)
+  
+  cart_result.each do |items, values|
+    # binding.pry
+      total = total + values[:price] * values[:count]
+    
+  end
+  
+  if total > 100
+    total *= 0.90
+  end
+
+  total
+
 end
