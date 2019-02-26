6f7820dcd03c170a8f79d1d6fdb40f68a2677f83
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-26 19:08:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,30 @@
 require "pry"
 
 def consolidate_cart(cart)
-
-  cart.each do |item|
-    binding.pry
+  consolidated_cart = {}
+  items_array = [] 
+  
+  items_array = cart.map {|items_hash| items_hash.keys}.flatten
+  
+  items_array.each do |item|
+    item_count = items_array.count(item)
+    
+    cart.each do |items_hash|
+      items_hash.each do |key, value|
+        
+        item_values = {}
+          if item == key
+            value[:count] = item_count
+            item_values = value
+            
+            consolidated_cart[item] = item_values
+            break       #breaks after first instance of item because all values for that item remain the same
+          end
+        end
+    end
   end
-  cart
+    
+  cart = consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
